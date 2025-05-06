1082f2f820a8d43e4447d0018d19b00a18982ac9
Gwimace-chan <screwyouphone32@gmail.com>
2025-05-06 18:55:51 -0500

    Test

--- GwimWare.lua
+++ GwimWare.lua
@@ -1,1 +1,17 @@
-print("Test")
+local Orion = loadstring(game:HttpGet(
+    "https://raw.githubusercontent.com/OrionLibrary/Orion/main/lib.lua",
+    true
+))()
+
+local Window = Orion:CreateWindow({ Name = "GwimWare Aimbot Test" })
+
+-- Aimbot Tab only
+local AimTab = Window:CreateTab("Aimbot")
+AimTab:CreateToggle({
+    Name = "Enable Aimbot",
+    CurrentValue = false,
+    Callback = function(v) print("Aimbot toggled:", v) end
+})
+
+Orion:Init()
+print("🎯 Aimbot tab loaded!")
